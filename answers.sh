87da7a96dd8bbbe00e8c3b6ae82ddda07f765541
ahmad badawi <ahmad.badawu12@gmail.com>
2021-12-13 14:47:58 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,14 @@
+.1
+SELECT names database2 FROM student ;
+.2
+SELECT * FROM student WHERE  age > 30;
+.3
+SELECT name FROM student WHERE  gender = "female" and age = 30;
+.4
+SELECT points FROM student WHERE  name ="Alex" ;
+.5
+INSERT INTO  student (Name , Age, Gender , Points) values ("assia","27","female","20");
+.6
+UPDATE  student SET points = points+1  WHERE  name ="Basma";
+.7
+UPDATE  student SET points = points-1  WHERE  name ="Alex";
